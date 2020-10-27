f0fd126e09c1be28a019ab8f6792c576984573da
vineela-max <vineelasatish@gmail.com>
2020-10-26 20:40:04 -0500

    add helloworld1 to git

---
+++ helloworld1/src/helloworld1/myproject.java
@@ -0,0 +1,11 @@
+package helloworld1;
+
+public class myproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my first project");
+
+	}
+
+}
